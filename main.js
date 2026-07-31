6ae93ca7fe17f693d3fb85b619119357cec9a163
Chan Nyein Aung <thesmartgooners1@gmail.com>
2026-08-01 03:11:41 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const menuBtn = document.getElementById("menuBtn");
+const mainNav = document.getElementById("mainNav");
+
+if (menuBtn && mainNav) {
+    menuBtn.addEventListener("click", () => {
+        mainNav.classList.toggle("open");
+    });
+}
