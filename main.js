c4deb1f7cfab1d7ce0d29e96bc23bf93dd16fcb8
Chan Nyein Aung <thesmartgooners1@gmail.com>
2026-08-01 03:32:58 +0700

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const menuBtn = document.getElementById("menuBtn");
-const mainNav = document.getElementById("mainNav");
-
-if (menuBtn && mainNav) {
-    menuBtn.addEventListener("click", () => {
-        mainNav.classList.toggle("open");
-    });
-}
